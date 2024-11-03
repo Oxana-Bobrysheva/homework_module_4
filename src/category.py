f07bea419b2404110769e2ea3abab0ba8f2c604f
Oxana Bobrysheva <bobrysheva_oxana@mail.ru>
2024-11-04 02:41:59 +0300

    Work if classes checked by main.py

--- src/category.py
+++ src/category.py
@@ -4,13 +4,13 @@ class Category:
     description: str
     products: list
 
-    quantity_of_category = 0
-    quantity_of_products = 0
+    category_count = 0
+    product_count = 0
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
         self.products = products
 
-        Category.quantity_of_category += 1
-        Category.quantity_of_products += 1
+        Category.category_count += 1
+        Category.product_count += len(products)
